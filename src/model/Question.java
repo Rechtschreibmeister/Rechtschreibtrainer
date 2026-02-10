9c17fdb741e4d9ec98b7545ffa321b14e4aa8901
stefan <sziehensack@student.tgm.ac.at>
2026-02-10 08:16:53 +0100

    Did some stuff

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.awt.*;
+
 public class Question {
+    private String question;
+    private Image image;
+    private String answer;
+
+    public Question(String question, Image image, String answer) {
+        this.question = question;
+        this.image = image;
+        this.answer = answer;
+    }
 }
