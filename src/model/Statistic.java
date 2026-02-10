d79d435f25d2c5be53e21e691d8859b285653bfb
lmueller <lmueller@student.tgm.ac.at>
2026-02-10 08:47:18 +0100

    Implemented QuizSL and added a new class Statistic

---
+++ src/model/Statistic.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Statistic {
+
+    private int questionsCorrect;
+    private int totalQuestions;
+
+    public Statistic() {
+        totalQuestions = 0;
+        questionsCorrect = 0;
+    }
+
+    public int getQuestionsCorrect() {
+        return questionsCorrect;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public int getWrongQuestions() {
+        return totalQuestions - questionsCorrect;
+    }
+
+    public double getCorrectPercentage(){
+        return (double) totalQuestions /(double) questionsCorrect *100;
+    }
+
+    public void addQuestion(boolean correctlyAwnsered){
+        totalQuestions++;
+        if(correctlyAwnsered){ questionsCorrect++;}
+    }
+
+}
