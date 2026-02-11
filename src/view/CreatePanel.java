61a86e456826f92635d95ce6b58763ae2a674422
lmueller <lmueller@student.tgm.ac.at>
2026-02-11 16:48:37 +0100

    Updated CreatePanel and StatisticsPanel

--- src/view/CreatePanel.java
+++ src/view/CreatePanel.java
@@ -1,4 +1,39 @@
 package view;
 
-public class CreatePanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class CreatePanel extends JPanel {
+
+    JTextField answer;
+    JTextField question;
+    JFileChooser image;
+
+    public CreatePanel(){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        answer = new JTextField();
+        question = new JTextField();
+        image = new JFileChooser();
+
+    }
+
+
+    public BufferedImage getImage() throws IOException {
+        File f = image.getSelectedFile();
+        return ImageIO.read(f);
+    }
+
+    public String getAnswer(){
+        return answer.getText();
+    }
+
+    public String getQuestion(){
+        return question.getText();
+    }
+
 }
--- src/view/StatisticsPanel.java
+++ src/view/StatisticsPanel.java
@@ -1,4 +1,25 @@
 package view;
 
-public class StatisticsPanel {
+import model.Statistic;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StatisticsPanel extends JPanel {
+
+
+    public StatisticsPanel(Statistic s) {
+        setLayout(new GridLayout(0, 1));
+        JLabel nQuestions = new JLabel("Gesamtanzahl der gestellten Fragen: " + s.getTotalQuestions());
+        JLabel rQuestions = new JLabel("Anzahl der Richtig beantworteten Fragen: " + s.getQuestionsCorrect());
+        JLabel wQuestions = new JLabel("Anzahl der falsch beantworteten Fragen: " + s.getWrongQuestions());
+        JLabel ratio = new JLabel("Prozent der richtig beantworteten Fragen: " + s.getCorrectPercentage());
+        add(nQuestions);
+        add(rQuestions);
+        add(wQuestions);
+        add(ratio);
+    }
+
+
+
 }
