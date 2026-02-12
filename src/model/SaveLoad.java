34ad7db4e1d2132fd2f3fb688e3736e1c36b51be
stefan <sziehensack@student.tgm.ac.at>
2026-02-12 15:38:10 +0100

    Merged SaveLoad into 1 class

--- src/model/SaveLoad.java
+++ src/model/SaveLoad.java
@@ -1,8 +1,38 @@
 package model;
 
-public interface SaveLoad {
+import java.io.*;
 
-    Quiz load(String name);
-    void save(Quiz q, String name);
+/**
+ * Diese Klasse kann ein Quiz-Objekt speichern bzw. laden.
+ *
+ * @author lmueller
+ * @version 2026-2-10
+ */
+public class SaveLoad {
+    private final String path;
 
+    public SaveLoad(String path) {
+        this.path = path;
+    }
+
+
+    public Quiz load(String filepath) {
+        Quiz q;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path + File.pathSeparator + filepath))) {
+            q = (Quiz) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return q;
+    }
+
+    public void save(String filepath, Quiz q) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path + File.pathSeparator + filepath))) {
+            oos.writeObject(q);
+        } catch (IOException e) {
+            System.out.println("Unable to save quiz: " + e.getMessage());
+            System.out.println(q);
+            throw new RuntimeException(e);
+        }
+    }
 }
