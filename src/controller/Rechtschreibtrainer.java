5158fd46c550217aa0ec1f07f9e3719bbb80edb6
lmueller <lmueller@student.tgm.ac.at>
2026-02-12 14:27:09 +0100

    implemented CreatePanel and changed the test-Controller to an interface

--- src/controller/Rechtschreibtrainer.java
+++ src/controller/Rechtschreibtrainer.java
@@ -1,9 +1,17 @@
 package controller;
 
+import view.Commands;
+import view.Frame;
+import view.MainPanel;
+
+import java.awt.event.ActionEvent;
+
 public class Rechtschreibtrainer implements Controller{
 
-    public Rechtschreibtrainer(){
+    Frame view;
 
+    public Rechtschreibtrainer(){
+        view = new Frame(this, new MainPanel(this));
     }
 
 
@@ -11,4 +19,32 @@ public class Rechtschreibtrainer implements Controller{
         Rechtschreibtrainer r = new Rechtschreibtrainer();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Commands c = null;
+        for(Commands c1 : Commands.values()){
+            if(e.getActionCommand().equals(c1.toString())){
+                c = c1;
+            }
+        }
+        System.out.println(c);
+        switch(c){
+            case quiz:
+
+                break;
+            case game:
+                break;
+            case stats:
+                break;
+            case create:
+                break;
+            case enter:
+                break;
+            case hint:
+                break;
+            case submit_question:
+                break;
+        }
+    }
 }
