034ad3ec64c2d38423c6a92d9e26689258c6ad38
stefan <sziehensack@student.tgm.ac.at>
2026-02-17 08:00:15 +0100

    Removed trailing spaces

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -23,7 +23,6 @@ public class Frame extends JFrame {
     public Frame(Controller controller, MainPanel panel) {
         super("Rechtschreibtrainer");
         this.mainpanel = panel;
-        Image.initImage();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1280, 720);
         this.setResizable(true);
